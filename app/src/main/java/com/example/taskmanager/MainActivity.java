e5685c736e5fadcc12570b087008ae1ca4a0c0dd
Jatin <jatin@Jatins-MacBook-Air.local>
2019-10-20 03:26:20 +0530

    added login functionality

--- app/src/main/java/com/example/taskmanager/MainActivity.java
+++ app/src/main/java/com/example/taskmanager/MainActivity.java
@@ -1,22 +1,92 @@
 package com.example.taskmanager;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView signup;
+
+    private EditText email;
+    private EditText pass;
+    private Button btnlogin;
+
+    //Firebase
+
+    private FirebaseAuth mAuth;
+
+    private ProgressDialog mDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mAuth = FirebaseAuth.getInstance();
+
+        mDialog = new ProgressDialog(this);
+
         signup = findViewById(R.id.signuptext);
+
+        email = findViewById(R.id.email);
+        pass = findViewById(R.id.password);
+        btnlogin = findViewById(R.id.loginbtn);
+
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String mEmail = email.getText().toString().trim();
+                String mPass = pass.getText().toString().trim();
+
+                if(TextUtils.isEmpty(mEmail)){
+                    email.setError("Required Field");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(mPass)){
+                    pass.setError("Required Field");
+                    return;
+                }
+
+                mDialog.setMessage("Processing");
+                mDialog.show();
+
+                mAuth.signInWithEmailAndPassword(mEmail,mPass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if(task.isSuccessful()){
+                            Toast.makeText(MainActivity.this, "Login Successful!", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),homeact.class));
+                            mDialog.dismiss();
+                        }
+                        else{
+                            Toast.makeText(MainActivity.this, "Problem!", Toast.LENGTH_SHORT).show();
+                            mDialog.dismiss();
+                        }
+
+                    }
+                });
+
+            }
+        });
 
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/example/taskmanager/homeact.java
+++ app/src/main/java/com/example/taskmanager/homeact.java
@@ -3,12 +3,20 @@ package com.example.taskmanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
 
 public class homeact extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homeact);
+
+        toolbar = findViewById(R.id.hometoolbar);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("TaskManager");
     }
 }
