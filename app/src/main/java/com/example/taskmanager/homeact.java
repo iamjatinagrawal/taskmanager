0dbc203918c27725d7c8498dd948f75a22450fa6
Jatin <jatin@Jatins-MacBook-Air.local>
2019-10-20 22:23:30 +0530

    fab button added

--- app/src/main/java/com/example/taskmanager/homeact.java
+++ app/src/main/java/com/example/taskmanager/homeact.java
@@ -3,11 +3,17 @@ package com.example.taskmanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class homeact extends AppCompatActivity {
 
     private Toolbar toolbar;
+
+    private FloatingActionButton btnfab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +24,15 @@ public class homeact extends AppCompatActivity {
 
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("TaskManager");
+
+        btnfab = findViewById(R.id.btnfab);
+
+        btnfab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
