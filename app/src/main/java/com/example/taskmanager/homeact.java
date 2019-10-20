be31a6641ceda02d617d52d20ec81e9c775d517f
Jatin <jatin@Jatins-MacBook-Air.local>
2019-10-21 00:02:23 +0530

    note adding functionality added

--- app/src/main/java/com/example/taskmanager/homeact.java
+++ app/src/main/java/com/example/taskmanager/homeact.java
@@ -1,8 +1,10 @@
 package com.example.taskmanager;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 
 import androidx.appcompat.widget.Toolbar;
@@ -30,6 +32,18 @@ public class homeact extends AppCompatActivity {
         btnfab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                AlertDialog.Builder mydialog = new AlertDialog.Builder(homeact.this);
+
+                LayoutInflater layoutInflater = LayoutInflater.from(homeact.this);
+
+                View custominputfieldview = layoutInflater.inflate(R.layout.custominputfield,null);
+
+                mydialog.setView(custominputfieldview);
+
+                AlertDialog dialog = mydialog.create();
+
+                dialog.show();
 
             }
         });
